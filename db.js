3feb2c6f905bed77f920046a1f817056cee2dc66
Ritesh Malik <99036166+Ritesh00007@users.noreply.github.com>
2024-05-27 15:24:10 +0530

    Update db.js
    Database production

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'ritesh',
+//     password: '280909', // Leave blank if not set
+//     port: 5432,
+// });
+
+// module.exports = pool;
 const { Pool } = require('pg');
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'ritesh',
-    password: '280909', // Leave blank if not set
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Necessary for Heroku's PostgreSQL
+  }
 });
 
 module.exports = pool;
